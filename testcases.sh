50d32997ec77108a61d86ca5cda762c4af930896
Quasic <qbasic010@aim.com>
2021-01-13 20:29:06 -0500

    testcases.sh: breaking api change, but simpler for most use cases

--- testcases.sh
+++ testcases.sh
@@ -4,13 +4,14 @@
 # Report bugs to https://github.com/Quasic/zdrop/issues
 # Released under Creative Commons Attribution (BY) 4.0 license
 printf '%s\n' "${BASH_SOURCE[0]} 0.1"
-if cd "$(dirname "${BASH_SOURCE[0]}")/TAP"
+if cd "$(dirname "${BASH_SOURCE[0]}")"&&[ -d TAP ]
 then
-	#shellcheck source=../TAP/TAP/updateTAP.bsh
-	[ -f ../../TAP/TAP/updateTAP.bsh ]&&source ../../TAP/TAP/updateTAP.bsh
-	#shellcheck source=TAP/testcases.bash
-	source testcases.bash zdrop "$@"
+	[ -f ../TAP/TAP/updateTAP.bsh ]&&
+		source ../TAP/TAP/updateTAP.bsh
+	source TAP/testcases.bash zdrop "$@"
 else
+	printf 'Could not find TAP folder...\n'
 	[[ "$-" = *i* ]]&&read -rn1 -p 'Press a key to exit 1'
 	exit 1
 fi
+printf '\n'
